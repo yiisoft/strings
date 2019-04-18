4dcf7f29a7d5236ec438c4711d21cb1e3ca12fa2
Mehdi Achour <machour@gmail.com>
2019-04-18 13:48:09 +0100

    Fix types

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -273,7 +273,7 @@ class StringHelper
      * @param float|int $number a floating point number or integer.
      * @return string the string representation of the number.
      */
-    public static function floatToString(int $number): string
+    public static function floatToString($number): string
     {
         // . and , are the only decimal separators known in ICU data,
         // so its safe to call str_replace here
