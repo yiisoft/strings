13ad016f24266adc84bfdc9eeb963cce5de15b1c
Viktor Babanov <viktorprogger@gmail.com>
2023-10-26 23:48:57 +0600

    Increase Wildcard Pattern speed (#117)
    Co-authored-by: Sergei Predvoditelev <sergei@predvoditelev.ru>

--- src/WildcardPattern.php
+++ src/WildcardPattern.php
@@ -15,7 +15,7 @@ use function strtr;
  *
  * - `\` escapes other special characters if usage of escape character is not turned off.
  * - `*` matches any string including the empty string except it has a delimiter (`/` and `\` by default).
-  * - `**` matches any string including the empty string and delimiters.
+ * - `**` matches any string including the empty string and delimiters.
  * - `?` matches any single character.
  * - `[seq]` matches any character in seq.
  * - `[a-z]` matches any character from a to z.
@@ -26,6 +26,11 @@ use function strtr;
 final class WildcardPattern
 {
     private bool $ignoreCase = false;
+
+    /**
+     * @psalm-var non-empty-string|null
+     */
+    private ?string $patternPrepared = null;
 
     /**
      * @param string $pattern The shell wildcard pattern to match against.
@@ -50,7 +55,55 @@ final class WildcardPattern
             return true;
         }
 
-        $pattern = $this->pattern;
+        return preg_match($this->getPatternPrepared(), $string) === 1;
+    }
+
+    /**
+     * Make pattern case insensitive.
+     */
+    public function ignoreCase(bool $flag = true): self
+    {
+        $new = clone $this;
+        $new->patternPrepared = null;
+        $new->ignoreCase = $flag;
+
+        return $new;
+    }
+
+    /**
+     * Returns whether the pattern contains a dynamic part i.e.
+     * has unescaped "*",  "{", "?", or "[" character.
+     *
+     * @param string $pattern The pattern to check.
+     *
+     * @return bool Whether the pattern contains a dynamic part.
+     */
+    public static function isDynamic(string $pattern): bool
+    {
+        $pattern = preg_replace('/\\\\./', '', $pattern);
+        return preg_match('/[*{?\[]/', $pattern) === 1;
+    }
+
+    /**
+     * Escapes pattern characters in a string.
+     *
+     * @param string $string Source string.
+     *
+     * @return string String with pattern characters escaped.
+     */
+    public static function quote(string $string): string
+    {
+        return preg_replace('#([\\\\?*\\[\\]])#', '\\\\$1', $string);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    private function getPatternPrepared(): string
+    {
+        if ($this->patternPrepared !== null) {
+            return $this->patternPrepared;
+        }
 
         $replacements = [
             '\*\*' => '.*',
@@ -81,49 +134,15 @@ final class WildcardPattern
             '\-' => '-',
         ];
 
-        $pattern = strtr(preg_quote($pattern, '#'), $replacements);
+        $pattern = strtr(preg_quote($this->pattern, '#'), $replacements);
         $pattern = '#^' . $pattern . '$#us';
 
         if ($this->ignoreCase) {
             $pattern .= 'i';
         }
 
-        return preg_match($pattern, $string) === 1;
-    }
+        $this->patternPrepared = $pattern;
 
-    /**
-     * Make pattern case insensitive.
-     */
-    public function ignoreCase(bool $flag = true): self
-    {
-        $new = clone $this;
-        $new->ignoreCase = $flag;
-        return $new;
-    }
-
-    /**
-     * Returns whether the pattern contains a dynamic part i.e.
-     * has unescaped "*",  "{", "?", or "[" character.
-     *
-     * @param string $pattern The pattern to check.
-     *
-     * @return bool Whether the pattern contains a dynamic part.
-     */
-    public static function isDynamic(string $pattern): bool
-    {
-        $pattern = preg_replace('/\\\\./', '', $pattern);
-        return preg_match('/[*{?\[]/', $pattern) === 1;
-    }
-
-    /**
-     * Escapes pattern characters in a string.
-     *
-     * @param string $string Source string.
-     *
-     * @return string String with pattern characters escaped.
-     */
-    public static function quote(string $string): string
-    {
-        return preg_replace('#([\\\\?*\\[\\]])#', '\\\\$1', $string);
+        return $this->patternPrepared;
     }
 }
