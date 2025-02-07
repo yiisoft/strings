c4d3f1811fe9f2337e425939a72a320f353d28f0
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-07 13:07:00 +0300

    narrow psalm type

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -489,7 +489,7 @@ final class StringHelper
      *
      * @return string[]
      *
-     * @psalm-return list<string>
+     * @psalm-return non-empty-list<string>
      */
     public static function parsePath(
         string $path,
