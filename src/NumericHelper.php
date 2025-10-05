01aae7716d5a5bfe33e70dccb895a6d60bec8cd3
Максим Спирков <spirkov.2001@mail.ru>
2025-10-05 13:40:23 +0400

    cr fix

--- src/NumericHelper.php
+++ src/NumericHelper.php
@@ -123,9 +123,11 @@ final class NumericHelper
     /**
      * Converts human readable size to bytes.
      *
-     * @param string $string human readable size. Examples: `1024`, `1kB`, `1.5M`, `1GiB`. Full
+     * @param string $string Human readable size. Examples: `1024`, `1kB`, `1.5M`, `1GiB`. Full
      * list of supported postfixes in {@see FILESYSTEM_SIZE_POSTFIXES}.
-     * Note: This parameter cannot be greater than `8192T`.
+     *
+     * Note: This parameter cannot be greater than `8192P - 1 byte` on 64-bit systems
+     * and `2G - 1 byte` on 32-bit systems.
      *
      * @throws InvalidArgumentException when the string is invalid.
      *
